93c4e62f4b604a7292e73aeb4e19ebb8943748cd
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2025-12-12 15:27:58 +0900

    テスト

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -6,6 +6,11 @@ using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	//===========================================================
+	// branch 1-1
+	//===========================================================
+
 	// ゲームシーンのインスタンス作成
 	GameScene* gameScene = new GameScene();
 
