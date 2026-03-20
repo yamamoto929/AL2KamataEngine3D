d9d78f6fd662582e8f02ebe4f47d3ad2602a1072
Yamamoto Runa <k025g0073@g.neec.ac.jp>
2026-03-21 01:54:32 +0900

    保存

--- DirectXGame/TitleScene.cpp
+++ DirectXGame/TitleScene.cpp
@@ -1,22 +1,38 @@
 #include "TitleScene.h"
+#include "WorldMatrixUpdate.h"
 using namespace KamataEngine;
 
 void TitleScene::Initialize() { 
 	modelPlayer_ = Model::CreateFromOBJ("player", true); 
 	modelTitleFont_ = Model::CreateFromOBJ("titleFont", true);
 
-	worldTransform_ .Initialize();
+	worldTransformPlayer_.Initialize();
+
+	worldTransformTitleFont_.Initialize();
 	
 
 	camera_ = new Camera();
 	camera_->Initialize();
 };
 
-void TitleScene::Update() {
+void TitleScene::Update() { 
+	if (Input::GetInstance()->PushKey(DIK_W)) {
+		worldTransformPlayer_.translation_.z -= 1.f;
+	} else if (Input::GetInstance()->PushKey(DIK_S)) {
+		worldTransformPlayer_.translation_.z += 1.f;
+	}
 
-};
+	WorldMatrixUpdate(worldTransformPlayer_);
+	WorldMatrixUpdate(worldTransformTitleFont_);
+
+	if (Input::GetInstance()->TriggerKey(DIK_SPACE)) {
+		DebugText::GetInstance()->ConsolePrintf("%f\n", worldTransformPlayer_.translation_.z);
+	}
+}
 
 void TitleScene::Draw() {
-	modelPlayer_->Draw(worldTransform_, *camera_);
-	modelTitleFont_->Draw(worldTransform_, *camera_);
+	Model::PreDraw();
+	modelPlayer_->Draw(worldTransformPlayer_, *camera_);
+	modelTitleFont_->Draw(worldTransformTitleFont_, *camera_);
+	Model::PostDraw();
 };
--- DirectXGame/TitleScene.h
+++ DirectXGame/TitleScene.h
@@ -5,7 +5,9 @@ private:
 	KamataEngine::Model* modelPlayer_ = nullptr;
 	KamataEngine::Model* modelTitleFont_ = nullptr;
 
-	KamataEngine::WorldTransform worldTransform_;
+	//KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::WorldTransform worldTransformPlayer_;
+	KamataEngine::WorldTransform worldTransformTitleFont_;
 	KamataEngine::Camera* camera_;
 
 public:
